02666b93f2a4998ae53362c010b3078751542b06
Niklas Sandhu <niklas.sandhu@gmail.com>
2019-08-15 21:26:51 +0200

    db

--- www/appscript.js
+++ www/appscript.js
@@ -1,4 +1,17 @@
 window.fn = {};
+var db = null;
+a = "";
+b = "";
+c = "";
+d = "";
+e = "";
+f = "";
+g = "";
+h = "";
+i = "";
+j = "";
+k = "";
+l = "";
 
 window.fn.open = function () {
   var menu = document.getElementById('menu');
@@ -27,231 +40,169 @@ var notify = function () {
 function checkRadio() {
   if (document.getElementById("q1.1").checked) {
     a = document.getElementById("q1.1").value;
-    document.getElementById("test").innerHTML = a;
   };
 
   if (document.getElementById("q1.2").checked) {
     a = document.getElementById("q1.2").value;
-    document.getElementById("test").innerHTML = a;
   };
 
   if (document.getElementById("q2.1").checked) {
     b = document.getElementById("q2.1").value;
-    document.getElementById("test").innerHTML = b;
   };
 
   if (document.getElementById("q2.2").checked) {
     b = document.getElementById("q2.2").value;
-    document.getElementById("test").innerHTML = b;
   };
 
   if (document.getElementById("q2.3").checked) {
     b = document.getElementById("q2.3").value;
-    document.getElementById("test").innerHTML = b;
   };
 
   if (document.getElementById("q3.1").checked) {
     c = document.getElementById("3.1").value;
-    document.getElementById("test").innerHTML = c;
   };
 
   if (document.getElementById("q3.2").checked) {
     c = document.getElementById("q3.2").value;
-    document.getElementById("test").innerHTML = c;
   };
 
   if (document.getElementById("q3.3").checked) {
     c = document.getElementById("q3.3").value;
-    document.getElementById("test").innerHTML = c;
   };
 
   if (document.getElementById("q3.4").checked) {
     c = document.getElementById("q3.4").value;
-    document.getElementById("test").innerHTML = c;
   };
 
   if (document.getElementById("q3.5").checked) {
     c = document.getElementById("q3.5").value;
-    document.getElementById("test").innerHTML = c;
   };
 
   if (document.getElementById("q4.1").checked) {
     d = document.getElementById("q4.1").value;
-    document.getElementById("test").innerHTML = d;
   };
 
   if (document.getElementById("q4.2").checked) {
     d = document.getElementById("q4.2").value;
-    document.getElementById("test").innerHTML = d;
   };
 
   if (document.getElementById("q5.1").checked) {
     e = document.getElementById("q5.1").value;
-    document.getElementById("test").innerHTML = e;
   };
 
   if (document.getElementById("q5.2").checked) {
     e = document.getElementById("q5.2").value;
-    document.getElementById("test").innerHTML = e;
   };
 
   if (document.getElementById("q5.3").checked) {
     e = document.getElementById("q5.3").value;
-    document.getElementById("test").innerHTML = e;
   };
 
   if (document.getElementById("q5.4").checked) {
     e = document.getElementById("q5.4").value;
-    document.getElementById("test").innerHTML = e;
   };
 
   if (document.getElementById("q6.1").checked) {
     f = document.getElementById("q6.1").value;
-    document.getElementById("test").innerHTML = f;
   };
 
   if (document.getElementById("q6.2").checked) {
     f = document.getElementById("q6.2").value;
-    document.getElementById("test").innerHTML = f;
   };
 
   if (document.getElementById("q6.3").checked) {
     f = document.getElementById("q6.3").value;
-    document.getElementById("test").innerHTML = f;
   };
 
   if (document.getElementById("q6.4").checked) {
     f = document.getElementById("q6.4").value;
-    document.getElementById("test").innerHTML = f;
   };
 
   if (document.getElementById("q7.1").checked) {
     g = document.getElementById("q7.1").value;
-    document.getElementById("test").innerHTML = g;
   };
 
   if (document.getElementById("q7.2").checked) {
     g = document.getElementById("q7.2").value;
-    document.getElementById("test").innerHTML = g;
   };
 
   if (document.getElementById("q7.3").checked) {
     g = document.getElementById("q7.3").value;
-    document.getElementById("test").innerHTML = g;
   };
 
   if (document.getElementById("q8.1").checked) {
     h = document.getElementById("q8.1").value;
-    document.getElementById("test").innerHTML = h;
   };
 
   if (document.getElementById("q8.2").checked) {
     h = document.getElementById("q8.2").value;
-    document.getElementById("test").innerHTML = h;
   };
 
   if (document.getElementById("q8.3").checked) {
     h = document.getElementById("q8.3").value;
-    document.getElementById("test").innerHTML = h;
   };
 
   if (document.getElementById("q8.4").checked) {
     h = document.getElementById("q8.4").value;
-    document.getElementById("test").innerHTML = h;
   };
 
   if (document.getElementById("q8.5").checked) {
     h = document.getElementById("q8.5").value;
-    document.getElementById("test").innerHTML = h;
   };
 
   if (document.getElementById("q9.1").checked) {
     i = document.getElementById("q9.1").value;
-    document.getElementById("test").innerHTML = i;
   };
 
   if (document.getElementById("q9.2").checked) {
     i = document.getElementById("q9.2").value;
-    document.getElementById("test").innerHTML = i;
   };
 
   if (document.getElementById("q9.3").checked) {
     i = document.getElementById("q9.3").value;
-    document.getElementById("test").innerHTML = i;
   };
 
   if (document.getElementById("q10.1").checked) {
     j = document.getElementById("q10.1").value;
-    document.getElementById("test").innerHTML = j;
   };
 
   if (document.getElementById("q10.2").checked) {
     j = document.getElementById("q10.2").value;
-    document.getElementById("test").innerHTML = j;
   };
 
   if (document.getElementById("q10.3").checked) {
     j = document.getElementById("q10.3").value;
-    document.getElementById("test").innerHTML = j;
   };
 
   if (document.getElementById("q11.1").checked) {
     k = document.getElementById("q11.1").value;
-    document.getElementById("test").innerHTML = k;
   };
 
   if (document.getElementById("q11.2").checked) {
     k = document.getElementById("q11.2").value;
-    document.getElementById("test").innerHTML = k;
   };
 
   if (document.getElementById("q11.3").checked) {
     k = document.getElementById("q11.3").value;
-    document.getElementById("test").innerHTML = k;
   };
 
   if (document.getElementById("q11.4").checked) {
     k = document.getElementById("q11.4").value;
-    document.getElementById("test").innerHTML = k;
   };
 
   if (document.getElementById("q11.5").checked) {
-    
     k = document.getElementById("q11.5").value;
-    document.getElementById("test").innerHTML = k;
   };
 
-  if (document.getElementById("q12.1").change) {
-    
     l = document.getElementById("q12.1").value;
     document.getElementById("test").innerHTML = l;
-  };
-
 }
-
-var createAlertDialog = function () {
-  var dialog = document.getElementById('my-alert-dialog');
-
-  if (dialog) {
-    dialog.show();
-  } else {
-    ons.createElement('alert-dialog.html', { append: true })
-      .then(function (dialog) {
-        dialog.show();
-      });
-  }
-
-  setTimeout(hideAlertDialog, 1000);
-
-};
 
 var hideAlertDialog = function () {
   document
-    .getElementById('my-alert-dialog')
+    .getElementById('alert-dialog')
     .hide();
 };
-
-var db = null;
 
 function onError(tx, e) {
   alert("Etwas ist fehlgeschlagen:" + e.Message);
@@ -262,16 +213,34 @@ function onSucces(tx, r) {
 }
 
 function openDb() {
-  db = openDatabase("SurveyOne", "1", "Wahl-Umfrage", 4096 * 4096);
+  db = openDatabase("Peter", "1", "Wahl-Umfrage", 4096 * 4096);
 
   db.transaction(function (tx) {
-    tx.executeSql("CREATE TABLE IF NOT EXISTS results (ID INTEGER PRIMARY KEY ASC, answer1 TEXT, answer2 TEXT, answer3 TEXT, answer4 TEXT, answer5 TEXT, answer6 TEXT, answer7 TEXT, answer8 TEXT, answer9 TEXT, answer10 TEXT, answer11 TEXT, answer12 TEXT", []);
+    tx.executeSql("CREATE TABLE IF NOT EXISTS klaus (ID INTEGER PRIMARY KEY ASC, answer1 TEXT, answer2 TEXT, answer3 TEXT, answer4 TEXT, answer5 TEXT, answer6 TEXT, answer7 TEXT, answer8 TEXT, answer9 TEXT, answer10 TEXT, answer11 TEXT, answer12 TEXT", []);
   });
+}
+
+function addData() {
+  db.transaction(function (tx) {
+    tx.executeSql("INSERT INTO klaus (answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10, answer11, answer12) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [a, b, c, d, e, f, g, h, i, j, k, l], onSucces, onError);
+  });
+  a = "";
+  b = "";
+  c = "";
+  d = "";
+  e = "";
+  f = "";
+  g = "";
+  h = "";
+  i = "";
+  j = "";
+  k = "";
+  l = "";
 }
 
 function getData() {
   db.transaction(function (tx) {
-    tx.executeSql("SELECT * FROM results", [], renderItems, onError)
+    tx.executeSql("SELECT * FROM klaus", [], renderItems, onError)
   });
 }
 
@@ -279,7 +248,7 @@ function renderItems(tx, r) {
   var output ="";
   var list = document.getElementById('ergebnisse_s1');
 
-  for(i = 0; i < resizeBy.rows.lenght; i++) {
+  for(i = 0; i < rs.rows.lenght; i++) {
     var row = rs.rows.answer1(i);
     output += "<ons-list-item>" + row.item + "</ons-list-item>";
   }
@@ -287,61 +256,20 @@ function renderItems(tx, r) {
   list.innerHTML = output;
 }
 
+var createAlertDialog = function () {
+  var dialog = document.getElementById('alert-dialog');
 
-function addData() {
+  if (dialog) {
+    dialog.show();
+  } else {
+    ons.createElement('alert-dialog.html', { append: true })
+      .then(function (dialog) {
+        dialog.show();
+      });
+  }
 
-  db.transaction(function (tx) {
-    tx.executeSql("INSERT INTO results (answer1) VALUES (?)", [a], onSucces, onError);
-  });
+  setTimeout(hideAlertDialog, 5000);
+  checkRadio();
+  addData();
 
-   {
-    tx.executeSql("INSERT INTO results (answer2) VALUES (?)", [b], onSucces, onError);
-  };
-
-   {
-    tx.executeSql("INSERT INTO results (answer3) VALUES (?)", [c], onSucces, onError);
-  };
-
-   {
-    tx.executeSql("INSERT INTO results (answer4) VALUES (?)", [d], onSucces, onError);
-  };
-
-   {
-    tx.executeSql("INSERT INTO results (answer5) VALUES (?)", [e], onSucces, onError);
-  };
-
-   {
-    tx.executeSql("INSERT INTO results (answer6) VALUES (?)", [f], onSucces, onError);
-  };
-
-   {
-    tx.executeSql("INSERT INTO results (answer7) VALUES (?)", [g], onSucces, onError);
-  };
-
-   {
-    tx.executeSql("INSERT INTO results (answer8) VALUES (?)", [h], onSucces, onError);
-  };
-
-   {
-    tx.executeSql("INSERT INTO results (answer9) VALUES (?)", [i], onSucces, onError);
-  };
-
-   {
-    tx.executeSql("INSERT INTO results (answer10) VALUES (?)", [j], onSucces, onError);
-  };
-
-   {
-    tx.executeSql("INSERT INTO results (answer11) VALUES (?)", [k], onSucces, onError);
-  };
-
-   {
-    tx.executeSql("INSERT INTO results (answer12) VALUES (?)", [l], onSucces, onError);
-  };
-
-
-
-
-
-
-  // reset var after input
-}
+};
