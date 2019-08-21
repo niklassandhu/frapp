7fc603923758d8786dce27b2436c2a6e255ee8a6
Niklas Sandhu <niklas.sandhu@gmail.com>
2019-08-21 08:15:57 +0200

    Update to Version 1.2 - NiklasSandhu

--- platforms/android/app/src/main/assets/www/appscript.js
+++ platforms/android/app/src/main/assets/www/appscript.js
@@ -1,4 +1,17 @@
 window.fn = {};
+var db = null;
+a = "";
+b = "";
+c = "";
+d = "";
+e = "";
+f = "";
+g = "";
+h = "";
+i = "";
+j = "";
+k = "";
+l = "";
 
 window.fn.open = function () {
   var menu = document.getElementById('menu');
@@ -15,6 +28,7 @@ window.fn.load = function (page) {
 
 document.addEventListener('init', function (event) {
   if (event.target.id == "home") {
+    openDb();
   }
 });
 
@@ -26,202 +40,216 @@ var notify = function () {
 function checkRadio() {
   if (document.getElementById("q1.1").checked) {
     a = document.getElementById("q1.1").value;
-    document.getElementById("test").innerHTML = a;
   };
 
   if (document.getElementById("q1.2").checked) {
     a = document.getElementById("q1.2").value;
-    document.getElementById("test").innerHTML = a;
   };
 
   if (document.getElementById("q2.1").checked) {
     b = document.getElementById("q2.1").value;
-    document.getElementById("test").innerHTML = b;
   };
 
   if (document.getElementById("q2.2").checked) {
     b = document.getElementById("q2.2").value;
-    document.getElementById("test").innerHTML = b;
   };
 
   if (document.getElementById("q2.3").checked) {
     b = document.getElementById("q2.3").value;
-    document.getElementById("test").innerHTML = b;
   };
 
   if (document.getElementById("q3.1").checked) {
-    c = document.getElementById("3.1").value;
-    document.getElementById("test").innerHTML = c;
+    c = document.getElementById("q3.1").value;
   };
 
   if (document.getElementById("q3.2").checked) {
     c = document.getElementById("q3.2").value;
-    document.getElementById("test").innerHTML = c;
   };
 
   if (document.getElementById("q3.3").checked) {
     c = document.getElementById("q3.3").value;
-    document.getElementById("test").innerHTML = c;
   };
 
   if (document.getElementById("q3.4").checked) {
     c = document.getElementById("q3.4").value;
-    document.getElementById("test").innerHTML = c;
   };
 
   if (document.getElementById("q3.5").checked) {
     c = document.getElementById("q3.5").value;
-    document.getElementById("test").innerHTML = c;
   };
 
   if (document.getElementById("q4.1").checked) {
     d = document.getElementById("q4.1").value;
-    document.getElementById("test").innerHTML = d;
   };
 
   if (document.getElementById("q4.2").checked) {
     d = document.getElementById("q4.2").value;
-    document.getElementById("test").innerHTML = d;
   };
 
   if (document.getElementById("q5.1").checked) {
     e = document.getElementById("q5.1").value;
-    document.getElementById("test").innerHTML = e;
   };
 
   if (document.getElementById("q5.2").checked) {
     e = document.getElementById("q5.2").value;
-    document.getElementById("test").innerHTML = e;
   };
 
   if (document.getElementById("q5.3").checked) {
     e = document.getElementById("q5.3").value;
-    document.getElementById("test").innerHTML = e;
   };
 
   if (document.getElementById("q5.4").checked) {
     e = document.getElementById("q5.4").value;
-    document.getElementById("test").innerHTML = e;
   };
 
   if (document.getElementById("q6.1").checked) {
     f = document.getElementById("q6.1").value;
-    document.getElementById("test").innerHTML = f;
   };
 
   if (document.getElementById("q6.2").checked) {
     f = document.getElementById("q6.2").value;
-    document.getElementById("test").innerHTML = f;
   };
 
   if (document.getElementById("q6.3").checked) {
     f = document.getElementById("q6.3").value;
-    document.getElementById("test").innerHTML = f;
   };
 
   if (document.getElementById("q6.4").checked) {
     f = document.getElementById("q6.4").value;
-    document.getElementById("test").innerHTML = f;
   };
 
   if (document.getElementById("q7.1").checked) {
     g = document.getElementById("q7.1").value;
-    document.getElementById("test").innerHTML = g;
   };
 
   if (document.getElementById("q7.2").checked) {
     g = document.getElementById("q7.2").value;
-    document.getElementById("test").innerHTML = g;
   };
 
   if (document.getElementById("q7.3").checked) {
     g = document.getElementById("q7.3").value;
-    document.getElementById("test").innerHTML = g;
   };
 
   if (document.getElementById("q8.1").checked) {
     h = document.getElementById("q8.1").value;
-    document.getElementById("test").innerHTML = h;
   };
 
   if (document.getElementById("q8.2").checked) {
     h = document.getElementById("q8.2").value;
-    document.getElementById("test").innerHTML = h;
   };
 
   if (document.getElementById("q8.3").checked) {
     h = document.getElementById("q8.3").value;
-    document.getElementById("test").innerHTML = h;
   };
 
   if (document.getElementById("q8.4").checked) {
     h = document.getElementById("q8.4").value;
-    document.getElementById("test").innerHTML = h;
   };
 
   if (document.getElementById("q8.5").checked) {
     h = document.getElementById("q8.5").value;
-    document.getElementById("test").innerHTML = h;
   };
 
   if (document.getElementById("q9.1").checked) {
     i = document.getElementById("q9.1").value;
-    document.getElementById("test").innerHTML = i;
   };
 
   if (document.getElementById("q9.2").checked) {
     i = document.getElementById("q9.2").value;
-    document.getElementById("test").innerHTML = i;
   };
 
   if (document.getElementById("q9.3").checked) {
     i = document.getElementById("q9.3").value;
-    document.getElementById("test").innerHTML = i;
   };
 
   if (document.getElementById("q10.1").checked) {
     j = document.getElementById("q10.1").value;
-    document.getElementById("test").innerHTML = j;
   };
 
   if (document.getElementById("q10.2").checked) {
     j = document.getElementById("q10.2").value;
-    document.getElementById("test").innerHTML = j;
   };
 
   if (document.getElementById("q10.3").checked) {
     j = document.getElementById("q10.3").value;
-    document.getElementById("test").innerHTML = j;
   };
 
   if (document.getElementById("q11.1").checked) {
     k = document.getElementById("q11.1").value;
-    document.getElementById("test").innerHTML = k;
   };
 
   if (document.getElementById("q11.2").checked) {
     k = document.getElementById("q11.2").value;
-    document.getElementById("test").innerHTML = k;
   };
 
   if (document.getElementById("q11.3").checked) {
     k = document.getElementById("q11.3").value;
-    document.getElementById("test").innerHTML = k;
   };
 
   if (document.getElementById("q11.4").checked) {
     k = document.getElementById("q11.4").value;
-    document.getElementById("test").innerHTML = k;
   };
 
   if (document.getElementById("q11.5").checked) {
     k = document.getElementById("q11.5").value;
-    document.getElementById("test").innerHTML = k;
   };
+
+  l = document.getElementById("q12.1").value;
+
+  setTimeout(addData(), 1000);
 }
 
+var hideAlertDialog = function () {
+  document.getElementById('alert-dialog')
+    .hide();
+  checkRadio();
+};
+
+function onError(tx, e) {
+  alert("Etwas ist fehlgeschlagen");
+}
+
+function onSucces(tx, r) {
+  alert("Vorgang erfolgreich");
+}
+
+function openDb() {
+  db = openDatabase("umfrage", "1", "Wahl-Umfrage", 100000);
+
+  db.transaction(function (tx) {
+    tx.executeSql("CREATE TABLE IF NOT EXISTS answers (ID INTEGER PRIMARY KEY ASC, answer1 TEXT, answer2 TEXT, answer3 TEXT, answer4 TEXT, answer5 TEXT, answer6 TEXT, answer7 TEXT, answer8 TEXT, answer9 TEXT, answer10 TEXT, answer11 TEXT, answer12 TEXT)", []);
+  });
+}
+
+function addData() {
+  db.transaction(function (tx) {
+    tx.executeSql("INSERT INTO answers (answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10, answer11, answer12) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [a, b, c, d, e, f, g, h, i, j, k, l], onSucces, onError);
+  });
+}
+
+
+function getData() {
+
+  db.transaction(function (tx) {
+    tx.executeSql('SELECT * FROM answers', [], function (tx, results) {
+      var len = results.rows.length, i;
+      document.getElementById('solution').innerHTML = "";
+      msg = "<p>Datensätze gefunden: " + len + "</p>";
+      document.getElementById('solution').innerHTML += msg;
+
+      for (i = 0; i < len; i++) {
+
+        msg = "<ons-row><ons-col><ons-list-item>" + results.rows.item(i).answer1 + "</ons-list-item></ons-col><ons-col><ons-list-item>" + results.rows.item(i).answer2 + "</ons-list-item></ons-col><ons-col><ons-list-item>" + results.rows.item(i).answer3 + "</ons-list-item></ons-col><ons-col><ons-list-item>" + results.rows.item(i).answer4 + "</ons-list-item></ons-col><ons-col><ons-list-item>" + results.rows.item(i).answer5 + "</ons-list-item></ons-col><ons-col><ons-list-item>" + results.rows.item(i).answer6 + "</ons-list-item></ons-col><ons-col><ons-list-item>" + results.rows.item(i).answer7 + "</ons-list-item></ons-col><ons-col><ons-list-item>" + results.rows.item(i).answer8 + "</ons-list-item></ons-col><ons-col><ons-list-item>" + results.rows.item(i).answer9 + "</ons-list-item></ons-col><ons-col><ons-list-item>" + results.rows.item(i).answer10 + "</ons-list-item></ons-col><ons-col><ons-list-item>" + results.rows.item(i).answer11 + "</ons-list-item></ons-col><ons-col><ons-list-item>" + results.rows.item(i).answer12 + "</ons-list-item></ons-col></ons-row>";
+        document.getElementById('solution').innerHTML += msg;
+        msg = "";
+      }
+    }, null);
+  });
+}
+
+
 var createAlertDialog = function () {
-  var dialog = document.getElementById('my-alert-dialog');
+  var dialog = document.getElementById('alert-dialog');
 
   if (dialog) {
     dialog.show();
@@ -232,51 +260,5 @@ var createAlertDialog = function () {
       });
   }
 
-  setTimeout(hideAlertDialog, 1000);
-
+  setTimeout(hideAlertDialog, 5000);
 };
-
-var hideAlertDialog = function () {
-  document
-    .getElementById('my-alert-dialog')
-    .hide();
-};
-
-var db = null;
-
-function onError(tx, e) {
-  alert("Etwas ist fehlgeschlagen:" + e.Message);
-}
-
-function onSucces(tx, r) {
-  alert("Vorgang erfolgreich:" + r.Message);
-}
-
-function openDb() {
-  var db = window.openDatabase("SurveyOne", "1", "Wahl-Umfrage", 1000000);
-
-  db.transaction(function (tx) {
-    tx.executeSql("CREATE TABLE IF NOT EXISTS results (ID INTEGER PRIMARY KEY ASC, answer1 TEXT", []);
-  });
-}
-
-function getData() {
-  db.transaction(function (tx) {
-    tx.executeSql("SELECT * FROM results", [], onSuccess, onError)
-  })
-}
-
-
-function addData() {
-  var answer = document.getElementById("q1.1")
-  var value = answer.value;
-
-  db.transaction(function (tx) {
-    tx.executeSql("INSERT INTO results (answer11) VALUES (?)", [value], onSucces, onError);
-  });
-
-  // reset var after input
-}
-// function renderItems(tx, r) {
-//   var output ="";
-// }
